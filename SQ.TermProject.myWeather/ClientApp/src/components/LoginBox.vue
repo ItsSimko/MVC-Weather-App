04f8820c1858ebe98076d4e876fabf72321fe48c
Alex Simko <asimko5312@conestogac.on.ca>
2023-12-05 03:30:08 -0500

    Made registration page and login page, need to make backend for it

--- SQ.TermProject.myWeather/ClientApp/src/components/LoginBox.vue
+++ SQ.TermProject.myWeather/ClientApp/src/components/LoginBox.vue
@@ -1,19 +1,11 @@
 ﻿<template>
-  <v-container fluid>
-    <v-row justify="center">
-      <v-col cols="12" sm="8" md="4">
-        <v-card class="login-card">
-          <v-card-title class="headline">Login</v-card-title>
-          <v-card-text>
-            <v-form @submit.prevent="login">
-              <v-text-field v-model="email" label="Email" outlined></v-text-field>
-              <v-text-field v-model="password" label="Password" type="password" outlined></v-text-field>
-              <v-btn color="primary" type="submit">Login</v-btn>
-            </v-form>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+  <v-container>
+    <v-form @submit.prevent="login">
+      <v-text-field v-model="email" label="Email" outlined></v-text-field>
+      <v-text-field v-model="password" label="Password" type="password" outlined></v-text-field>
+      <v-btn color="primary" type="submit">Login</v-btn>
+      <v-btn color="primary" @click="goToReg" class="mx-1">Register</v-btn>
+    </v-form>
   </v-container>
 </template>
 
@@ -27,7 +19,11 @@
     },
     methods: {
       login() {
+        // make call to backend
+      },
 
+      goToReg() {
+        this.$router.push('/admin/register');
       }
     }
   };
---
+++ SQ.TermProject.myWeather/ClientApp/src/components/RegistrationBox.vue
@@ -0,0 +1,64 @@
+﻿<template>
+  <v-container>
+    <v-form @submit.prevent="registerUser">
+      <v-text-field v-model="username"
+                    label="Username"
+                    required></v-text-field>
+
+      <v-text-field v-model="email"
+                    label="Email"
+                    required
+                    :rules="emailRules"></v-text-field>
+
+      <v-text-field v-model="password"
+                    label="Password"
+                    type="password"
+                    required
+                    :rules="passwordRules"></v-text-field>
+
+      <v-text-field v-model="confirmPassword"
+                    label="Confirm Password"
+                    type="password"
+                    required
+                    :rules="confirmPasswordRules"></v-text-field>
+
+      <v-text-field v-model="validationCode"
+                    label="Please enter your the code provided by the superadmin."
+                    type="password"
+                    required></v-text-field>
+
+      <v-btn type="submit" color="primary">Register</v-btn>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      validationCode: '',
+      emailRules: [
+        v => !!v || 'Email is required',
+        v => /.+@.+\..+/.test(v) || 'Email must be valid',
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => (v && v.length >= 8) || 'Password must be at least 8 characters',
+      ],
+      confirmPasswordRules: [
+        v => !!v || 'Please confirm your password',
+        v => v === this.password || 'Passwords must match',
+      ],
+    };
+  },
+  methods: {
+    registerUser() {
+      // send to server logic
+    },
+  },
+};
+</script>
