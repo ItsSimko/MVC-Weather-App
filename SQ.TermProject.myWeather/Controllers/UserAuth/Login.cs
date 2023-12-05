80b8db5fc014ab903b38788609fcc7c1e0d76c58
Alex Simko <asimko5312@conestogac.on.ca>
2023-12-05 13:16:10 -0500

    Added the backend login call

---
+++ SQ.TermProject.myWeather/Controllers/UserAuth/Login.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SQ.TermProject.myWeather.Models;
+using SQ.TermProject.myWeather.Services;
+using System.Text.Json;
+
+
+namespace SQ.TermProject.myWeather.Controllers.UserAuth
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Login : ControllerBase
+    {
+        private UserService userService = new UserService();
+
+        [HttpGet(Name = "LoginUser")]
+        public IActionResult LoginUser(string username, string password)
+        {
+            if (userService.IsValidUser(username, password))
+            {
+                // Authentication successful
+                // session identifier for the user
+                string authToken = GenerateAuthToken();
+
+                // Return a success response with the token 
+                var resp = new { success = true, token = authToken };
+                return Content(JsonSerializer.Serialize(resp), "application/json");
+
+            }
+            else
+            {
+                // Invalid credentials
+                var resp = new { success = false, message = "Invalid username or password" };
+                return Content(JsonSerializer.Serialize(resp), "application/json");
+            }
+        }
+
+
+
+        public static string GenerateAuthToken()
+        {
+
+            return null;
+        }
+
+    }
+}
