c430475e9c7a46548ba18ecac668dbc5820b073d
Alex Simko <asimko5312@conestogac.on.ca>
2023-12-05 02:45:56 -0500

    created password serivce with base salt and hash functions

---
+++ SQ.TermProject.myWeather/Services/PasswordService.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SQ.TermProject.myWeather.Services
+{
+    public class PasswordService
+    {
+        private const int SaltLength = 16;
+
+        public static byte[] SaltHashBytes(byte[] passwordBytes, byte[] salt)
+        {
+            byte[] combinedBytes = new byte[passwordBytes.Length + salt.Length];
+            Buffer.BlockCopy(passwordBytes, 0, combinedBytes, 0, passwordBytes.Length);
+            Buffer.BlockCopy(salt, 0, combinedBytes, passwordBytes.Length, salt.Length);
+            return combinedBytes;
+        }
+
+        public static byte[] GenerateSalt(int length)
+        {
+            byte[] salt = new byte[length];
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetNonZeroBytes(salt);
+            }
+            return salt;
+        }
+
+        public static string GetSha256Hash(string input)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2")); 
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
