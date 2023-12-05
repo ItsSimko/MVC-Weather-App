01a1132606e17b19794f98ef7cbec9da8abf786f
Alex Simko <asimko5312@conestogac.on.ca>
2023-12-05 02:46:58 -0500

    Created the UI login for the admin page, and fixed some router and component pathing

--- SQ.TermProject.myWeather/ClientApp/src/App.vue
+++ SQ.TermProject.myWeather/ClientApp/src/App.vue
@@ -5,22 +5,24 @@
 
 <template>
   <v-app>
-    <v-main class="bg-setup">
-        <SearchBar class="w-25 mx-auto my-10 bg-blue-grey-darken-4
-        pa-3 rounded-xl " style="position: absoulte;"></SearchBar>
-        <RouterView class="h-50 bg-transparent"/>
+    <v-main>
+      <v-row>
+        <v-col>
+          <RouterView />
+        </v-col>
+      </v-row>
     </v-main>
   </v-app>
 </template>
 
 
 <style>
-.bg-setup {
-  background: url('./assets/bg/cloudy.jpg') no-repeat center center fixed !important;
-  background-size: cover;
-}
 
-.v-application--wrap {
-  min-height: 0vh !important;
-}
+  * {
+    margin: 0;
+  }
+
+  .v-application--wrap {
+    min-height: 0vh !important;
+  }
 </style>
