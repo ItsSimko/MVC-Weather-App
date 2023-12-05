e3f79611448b899cf46d2d5eb766a4d3157f22ed
Alex Simko <asimko5312@conestogac.on.ca>
2023-12-05 11:35:56 -0500

    Created database context, and some datamodels

---
+++ SQ.TermProject.myWeather/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace SQ.TermProject.myWeather.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+
+        public UserRole Role { get; set; }
+
+    }
+}
