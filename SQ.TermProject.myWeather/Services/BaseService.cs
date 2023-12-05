23489df5c16395a2fde353e32467f9c600849d93
Alex Simko <asimko5312@conestogac.on.ca>
2023-12-05 13:04:02 -0500

    Created base serivce, so all services have db access

---
+++ SQ.TermProject.myWeather/Services/BaseService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SQ.TermProject.myWeather.Database;
+
+namespace SQ.TermProject.myWeather.Services
+{
+    public class BaseService
+    {
+        public AppDbContext DbContext { get; set; }
+
+        public BaseService() 
+        { 
+            DbContext = new AppDbContext();
+            DbContext.Database.EnsureCreated();
+        }
+    }
+}
