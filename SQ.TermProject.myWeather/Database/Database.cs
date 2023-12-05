661233db1493d0528ace95a6b4823fdf8099fd0c
Alex Simko <asimko5312@conestogac.on.ca>
2023-12-05 11:35:56 -0500

    Created database context, and some datamodels

---
+++ SQ.TermProject.myWeather/Database/Database.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
+using SQ.TermProject.myWeather.Models;
+
+namespace SQ.TermProject.myWeather.Database
+{
+    public class Database : DbContext
+    {
+        public Database() { }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            const string connectionString = "server=localhost;port=3306;database=WeatherApp;user=root;password=2025";
+
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.UserName).IsRequired();
+                entity.Property(e => e.Email).IsRequired();
+                entity.Property(e => e.Password).IsRequired();
+                entity.Property(e => e.Salt).IsRequired();
+            });
+
+            modelBuilder.Entity<UserRole>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Name).IsRequired();
+                entity.Property(e => e.PermissionLevel).IsRequired();
+            });
+
+
+
+
+
+        }
+    }
+}
+
---
+++ SQ.TermProject.myWeather/Models/UserRole.cs
@@ -0,0 +1,16 @@
+﻿namespace SQ.TermProject.myWeather.Models
+{
+    public class UserRole
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int PermissionLevel { get; set; }
+
+        public UserRole ()
+        {
+            Name = "User";
+            PermissionLevel = 0;
+        }
+    }   
+}
