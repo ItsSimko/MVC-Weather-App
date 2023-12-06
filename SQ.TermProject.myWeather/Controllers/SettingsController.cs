cbe0466208cac73a828b2eeca65d6e76d9017c3d
Alex Simko <asimko5312@conestogac.on.ca>
2023-12-06 15:11:29 -0500

    Added settings controller, and get all users func

---
+++ SQ.TermProject.myWeather/Controllers/SettingsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQ.TermProject.myWeather.Models;
+using SQ.TermProject.myWeather.Services;
+
+namespace SQ.TermProject.myWeather.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class SettingsController : ControllerBase
+    {
+        private SettingsService settingsService = new SettingsService();
+
+        [HttpGet(Name = "GetUsers")]
+        public ActionResult<IEnumerable<User>> GetUsers(string username)
+        {
+            var users = settingsService.DbContext.Users.Where(u => EF.Functions.Like(u.UserName, $"{username}%")).ToList();
+            return Ok(users);
+        }
+    }
+}
