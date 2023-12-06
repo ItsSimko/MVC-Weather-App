563cae8be2e7d19b2672026cdda115854794dec9
Alex Simko <asimko5312@conestogac.on.ca>
2023-12-06 15:51:01 -0500

    Made settigns controller query all users

--- SQ.TermProject.myWeather/Controllers/SettingsController.cs
+++ SQ.TermProject.myWeather/Controllers/SettingsController.cs
@@ -12,10 +12,25 @@ namespace SQ.TermProject.myWeather.Controllers
         private SettingsService settingsService = new SettingsService();
 
         [HttpPost("GetUsers")]
-        public ActionResult<IEnumerable<User>> GetUsers(string username)
+        public ActionResult<IEnumerable<object>> GetUsers(string username)
         {
-            var users = settingsService.DbContext.Users.Where(u => EF.Functions.Like(u.UserName, $"{username}%")).ToList();
-            return Ok(users);
+
+            object usersWithRoles = from u in settingsService.DbContext.Users
+                                 join ur in settingsService.DbContext.UserRoles
+                                 on u.RoleId equals ur.Id
+                                 where u.UserName.StartsWith(username)
+
+                                 select new
+                                 {
+                                     u.Id,
+                                     u.UserName,
+                                     ur.Name,
+                                     u.Email
+                                     // Include other properties you want to retrieve
+                                 };
+
+
+            return Ok(usersWithRoles);
         }
     }
 }
