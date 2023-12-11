698afd5d588e38603e6500102d0ed61ac11bebcc
Alex Simko <asimko5312@conestogac.on.ca>
2023-12-11 14:07:20 -0500

    Moved all weather data to classes instead of dynamic, and made ui change all data from c to f on change

---
+++ SQ.TermProject.myWeather/Models/WeatherData/Snow.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace SQ.TermProject.myWeather.Models.WeatherData
+{
+    public class Snow
+    {
+        [JsonProperty("1h")]
+        public double _1h { get; set; }
+    }
+}
