375f979337d50efe6481d6fb2730e9f6198e02ff
Alex Simko <asimko5312@conestogac.on.ca>
2023-12-11 05:21:01 -0500

    Moved frontend api call to searchbar component and displayed rest of wweather data in additional weather container

--- SQ.TermProject.myWeather/ClientApp/src/components/WeatherInformation.vue
+++ SQ.TermProject.myWeather/ClientApp/src/components/WeatherInformation.vue
@@ -1,114 +1,180 @@
 <script setup lang="ts">
   import { ref, watch, reactive, toRefs } from "vue";
-  import { myLoc } from "./SearchBar.vue";
-  import axios from "axios";
+  import { myLoc, weatherData } from "./SearchBar.vue";
 
-  let temp = ref(23);
 
   let location = myLoc;
+  let weather = weatherData;
 
-  let weatherData = reactive({
-    "coord": {
-      "lon": null,
-      "lat": null
-    },
-    "weather": [
-      {
-        "id": null,
-        "main": null,
-        "description": null,
-        "icon": null
-      }
-    ],
-    "base": "stations",
-    "main": {
-      "temp": null,
-      "feels_like": null,
-      "temp_min": null,
-      "temp_max": null,
-      "pressure": null,
-      "humidity": null,
-    },
-    "visibility": null,
-    "wind": {
-      "speed": null,
-      "deg": null,
-    },
-    "clouds": {
-      "all": null,
-    },
-    "dt": null,
-    "sys": {
-      "type": null,
-      "id": null,
-      "country": null,
-      "sunrise": null,
-      "sunset": null,
-    },
-    "timezone": null,
-    "id": null,
-    "name": null,
-    "cod": null,
-  });
+  let currentMetric = 'C';
 
+  function changeMetric() {
+    if (currentMetric === 'C') {
+      weather.data.main.temp = (weather.data.main.temp * (9 / 5)) + 32
+      currentMetric = "F"
+    }
+    else if (currentMetric === 'F') {
+      currentMetric = "C"
+      weather.data.main.temp = (weather.data.main.temp - 32) * (5 / 9);
 
-
-  const myValue = ref(myLoc)
-
-  watch(() => myValue, (newValue, oldValue) => {
-    console.log(myLoc)
-    axios.post("/api/WeatherForecast/GetWeather?cityName=" + newValue.myLocationT.name).then(resp => {
-      weatherData = resp.data;
-      console.log(resp)
-    });
-  });
-
+    }
+  }
 
 </script>
 
-<script lang="ts">
 
 
-
-
-</script>
 
 <template>
-    <v-container class="bg-blue-grey-darken-4 rounded-lg w-70 p-6" >
-        <v-row class="bg-transparent ma-3">
-            <p class="text-h3 mx-auto">{{ location.myLocationT.name }}, {{location.myLocationT.country}}</p>
-        </v-row>
-        <v-row no-gutters class="ma-2 py-2">
-            <v-col cols="4" class="mx-auto mx-1">
-                <v-sheet class="bg-transparent">
-                    <v-row no-gutters class="">
-                        <v-col cols="9">
-                            <v-sheet class="bg-transparent">
-                                <p class="mx-auto text-center text-h4 my-2">Current Weather</p>
-                            </v-sheet>
-                        </v-col>
-                        <v-col cols="3">
-                            <v-sheet class="bg-transparent">
-                                <v-switch label="C⇔F" class="mx-auto"></v-switch>
-                            </v-sheet>
-                        </v-col>
-                    </v-row>
-                    <v-row >
-                      <p class="text-h1 mx-auto my-2">{{weatherData}}° C</p>
-                    </v-row>
-                </v-sheet>
+
+  <v-container class="bg-blue-grey-darken-4 rounded-lg w-70 p-6" v-if="weather.data">
+    <v-row class="bg-transparent ma-3">
+      <p class="text-h3 mx-auto">{{ location.myLocationT.name }}, {{location.myLocationT.country}}</p>
+    </v-row>
+    <v-row no-gutters class="ma-2 py-2">
+      <v-col cols="4" class="mx-auto mx-1">
+        <v-sheet class="bg-transparent">
+          <v-row no-gutters class="">
+            <v-col cols="9">
+              <v-sheet class="bg-transparent">
+                <p class="mx-auto text-center text-h4 my-2">Current Weather</p>
+              </v-sheet>
             </v-col>
-            <v-col class="bg-transparent mx-1 border border-primary">
-                <v-sheet class="bg-transparent">
-                    <p class="text-h5"> Additional Weather data goes here... </p>
-                </v-sheet>
+            <v-col cols="3">
+              <v-sheet class="bg-transparent">
+                <v-switch @change="changeMetric" label="C⇔F" class="mx-auto"></v-switch>
+              </v-sheet>
             </v-col>
-        </v-row>
-    </v-container>
+          </v-row>
+          <v-row>
+            <p class="text-h1 mx-auto my-2">{{weather.data.main.temp.toFixed(2)}}° {{currentMetric}}</p>
+          </v-row>
+        </v-sheet>
+      </v-col>
+      <v-col class="bg-transparent mx-1">
+        <v-sheet class="bg-transparent">
+          <v-row>
+            <v-col>
+              <v-sheet class="bg-transparent">
+                <v-row class="mx-auto text-center">
+                  <v-col>
+                    <v-sheet class="bg-transparent">
+                      <p> Feels Like:</p>
+                    </v-sheet>
+                  </v-col>
+                </v-row>
+                <v-row class="mx-auto text-center">
+                  <v-col>
+                    <v-sheet class="bg-transparent">
+                      <p class="text-h4"> {{ weather.data.main.feels_like }}</p>
+                    </v-sheet>
+                  </v-col>
+                </v-row>
+              </v-sheet>
+            </v-col>
+            <v-col>
+              <v-sheet class="bg-transparent">
+                <v-row class="mx-auto text-center">
+                  <v-col>
+                    <v-sheet class="bg-transparent">
+                      <p> Humidity:</p>
+                    </v-sheet>
+                  </v-col>
+                </v-row>
+                <v-row class="mx-auto text-center">
+                  <v-col>
+                    <v-sheet class="bg-transparent">
+                      <p class="text-h4"> {{ weather.data.main.humidity }}</p>
+                    </v-sheet>
+                  </v-col>
+                </v-row>
+              </v-sheet>
+            </v-col>
+            <v-col>
+              <v-sheet class="bg-transparent">
+                <v-row class="mx-auto text-center">
+                  <v-col>
+                    <v-sheet class="bg-transparent">
+                      <p > Cloud Coverage:</p>
+                    </v-sheet>
+                  </v-col>
+                </v-row>
+                <v-row class="mx-auto text-center">
+                  <v-col>
+                    <v-sheet class="bg-transparent">
+                      <p class="text-h4"> {{ weather.data.clouds.all }}%</p>
+                    </v-sheet>
+                  </v-col>
+                </v-row>
+              </v-sheet>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col>
+              <v-sheet class="bg-transparent">
+                <v-row class="mx-auto text-center">
+                  <v-col>
+                    <v-sheet class="bg-transparent">
+                      <p> Daily High:</p>
+                    </v-sheet>
+                  </v-col>
+                </v-row>
+                <v-row class="mx-auto text-center">
+                  <v-col>
+                    <v-sheet class="bg-transparent">
+                      <p class="text-h4"> {{ weather.data.main.temp_max }}</p>
+                    </v-sheet>
+                  </v-col>
+                </v-row>
+              </v-sheet>
+            </v-col>
+            <v-col>
+              <v-sheet class="bg-transparent">
+                <v-row class="mx-auto text-center">
+                  <v-col>
+                    <v-sheet class="bg-transparent"> 
+                      <p> Daily Low:</p>
+                    </v-sheet>
+                  </v-col>
+                </v-row>
+                <v-row class="mx-auto text-center">
+                  <v-col>
+                    <v-sheet class="bg-transparent">
+                      <p class="text-h4"> {{ weather.data.main.temp_min }}</p>
+                    </v-sheet>
+                  </v-col>
+                </v-row>
+              </v-sheet>
+            </v-col>
+            <v-col>
+              <v-sheet class="bg-transparent">
+                <v-row class="mx-auto text-center">
+                  <v-col>
+                    <v-sheet class="bg-transparent">
+                      <p> Pressure:</p>
+                    </v-sheet>
+                  </v-col>
+                </v-row>
+                <v-row class="mx-auto text-center">
+                  <v-col>
+                    <v-sheet class="bg-transparent">
+                      <p class="text-h4"> {{ weather.data.main.pressure }}</p>
+                    </v-sheet>
+                  </v-col>
+                </v-row>
+              </v-sheet>
+            </v-col>
+          </v-row>
+
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<style>
+<style scoped>
 v-sheet {
-    background-color: transparent;
+    background-color: transparent!important;
 }
 </style>
