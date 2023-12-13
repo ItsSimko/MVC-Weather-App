fecd098888b1fadc2d94962fc4ab57cd5e36e508
Alex Simko <asimko5312@conestogac.on.ca>
2023-12-13 09:44:27 -0500

    More tests and some ui ffixes for funcitonality

---
+++ UnitTest/StatTest.cs
@@ -0,0 +1,45 @@
+﻿using SQ.TermProject.myWeather.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class StatTest
+    {
+        private StatisticService statisticService;
+        public StatTest() 
+        {
+            statisticService = new StatisticService();
+        }
+
+        [TestMethod]
+        public void GetTestSearchCount_SampleSearch_KnowCount()
+        {
+            string cityInput = "testCity";
+
+            int expected = -23;
+
+            int act = statisticService.GetStat(cityInput);
+
+            Assert.AreEqual(expected, act);
+
+        }
+
+        [TestMethod]
+        public void GetWrongTestSearchCount_SampleSearch_IncorrectCount()
+        {
+            string cityInput = "testCity";
+
+            int expected = 25;
+
+            int act = statisticService.GetStat(cityInput);
+
+            Assert.AreNotEqual(expected, act);
+
+        }
+    }
+}
---
+++ UnitTest/UserServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using SQ.TermProject.myWeather.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UserServiceTests
+    {
+        private UserService UserService { get; set; }
+
+        public UserServiceTests() 
+        {
+            UserService = new UserService();
+        }
+
+        [TestMethod]
+        public void CheckUserRole_UserAndRole_CorrectUserRole()
+        {
+                string userName = "AlexSimko";
+
+                string expectedRole = "Superadmin";
+
+                string receivedRole = UserService.getUserRole(userName);
+
+                Assert.AreEqual(expectedRole, receivedRole);
+        }
+    }
+}
